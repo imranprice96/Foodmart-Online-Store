c9ce4ec733ddaa6312f4339b71e8699224f4c38c
iprice <imranprice96@gmail.com>
2019-02-09 20:02:18 +1300

    css

--- Foodmart/App_Start/BundleConfig.cs
+++ Foodmart/App_Start/BundleConfig.cs
@@ -26,10 +26,11 @@ namespace Foodmart
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/store.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/store.css",
+                        "~/Content/navigation.css",
                         "~/Content/reset.css"));
+
         }
     }
 }
